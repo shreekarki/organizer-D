af9479239bf80feb417ed1b8a525360efc688bea
karki <shristy.karki@tomtom.com>
2019-11-01 12:19:39 +0100

    DEleted all the file

--- oragnizer-D/src/app/department.ts
+++
@@ -1,7 +0,0 @@
-export class Department {
-  constructor(
-    public id: number,
-    public name: string,
-    public building: string,
-  ) {}
-}
--- oragnizer-D/src/app/task.ts
+++
@@ -1,8 +0,0 @@
-export class Task {
-  constructor(
-    public id: number,
-    public name: string,
-    public dueDate: string,
-    public complete: boolean,
-  ) {}
-}
