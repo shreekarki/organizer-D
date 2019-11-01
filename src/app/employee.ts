577f1ac3a1f85e64b2c48f8760c5b2b78c0cb54f
karki <shristy.karki@tomtom.com>
2019-11-01 12:22:32 +0100

    Added necessary file

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+  constructor(
+    public id: number,
+    public first_name: string,
+    public last_name: string,
+    public birth_date: string,
+    public department_id: number,
+  ) {}
+}
+
